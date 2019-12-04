51707cc89cb13c3d20764128b1cea551ada34686
Andrew Dempsey <andrewdempsey2016@gmail.com>
2019-12-04 18:03:42 +0000

    Setup database connection

--- app.py
+++ app.py
@@ -1,6 +1,19 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, redirect, request, url_for
+from flask_pymongo import PyMongo
+from bson.objectid import ObjectId
 
 app = Flask(__name__)
+
+#Connect to the database that holds the games information
+
+app.config["MONGO_DBNAME"] = "indie_flaunt"
+
+#note remove this (username&password) and use env variable instead
+
+app.config["MONGO_URI"] = "mongodb+srv://root:r00tUser@andrewcluster-igjjx.mongodb.net/indie_flaunt?retryWrites=true&w=majority"
+
+#make an instance of PyMongo and pass the app in
+mongo = PyMongo(app)
 
 @app.route("/")
 def index():
@@ -21,3 +34,7 @@ def sports():
 @app.route("/educational")
 def educational():
     return render_template("educational.html")
+
+@app.route("/get_games")
+def get_games():
+    return render_template("test.html", games = mongo.db.games.find())
